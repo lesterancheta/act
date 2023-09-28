ec72c99c19f28485ec0d1f4f1be85c115ff6ef74
lesterancheta <130680522+lesterancheta@users.noreply.github.com>
2023-09-28 23:12:31 +0800

    ahhmm

---
+++ app/Views/include/footer.php
@@ -0,0 +1,4 @@
+<div id="footer">
+  <div class="left"> <a href="#">Home</a> <span>|</span> <a href="#">Support</a> <span>|</span> <a href="#">My Account</a> <span>|</span> <a href="#">The Store</a> <span>|</span> <a href="#">Contact</a> </div>
+  <div class="right"> &copy; Sitename.com. Design by <a href="http://chocotemplates.com">ChocoTemplates.com</a> </div>
+</div>
